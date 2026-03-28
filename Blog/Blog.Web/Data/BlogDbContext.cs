af0aaf2b812b9083fb21b6c6b552aac91ae9aa06
Mubashar Yasin <mubasharraj012@gmail.com>
2026-03-29 01:53:58 +0500

    Inject DbContext into our Web-App
    Database connectivity with the project has been completed.

--- Blog/Blog.Web/Data/BlogDbContext.cs
+++ Blog/Blog.Web/Data/BlogDbContext.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Web.Data
 {
-    public class BlogDbContext :DbContext
+    public class BlogDbContext : DbContext
     {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
 
+        }
+
+        public DbSet<BlogPost> tbl_BlogPost { get; set; }
+
+        public DbSet<Tag> tbl_Tags { get; set; }
     }
 }
