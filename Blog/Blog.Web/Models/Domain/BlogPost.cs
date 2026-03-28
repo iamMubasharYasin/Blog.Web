c3e5fe00855d389e0d9c5d9ddf4953e34dcfabe8
Mubashar Yasin <mubasharraj012@gmail.com>
2026-03-29 01:06:16 +0500

    Add Domain Models
    BlogPost and Tag are created as domain models and a (M:N) is established between them.

---
+++ Blog/Blog.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿namespace Blog.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageURL { get; set; }
+        public string URLHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public Boolean Visible { get; set; }
+
+        //Many-to-Many RelationShip
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ Blog/Blog.Web/Models/Domain/Tag.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        //Many-to-Many RelationShip
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
