8025ed8fef60f7c9b03db751385c29234712e25b
Mubashar Yasin <mubasharraj012@gmail.com>
2026-04-12 12:23:13 +0500

    Working AdminTagsController.cs
    Saving Data to Database using DbContext

--- Blog/Blog.Web/Controllers/AdminTagsController.cs
+++ Blog/Blog.Web/Controllers/AdminTagsController.cs
@@ -1,10 +1,18 @@
-﻿using Blog.Web.Models.ViewModels;
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+       private BlogDbContext _blogdbcontext;
+        public AdminTagsController(BlogDbContext blogdbcontext)
+        {
+            _blogdbcontext = blogdbcontext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -15,11 +23,15 @@ namespace Blog.Web.Controllers
         [ActionName("Add")]
         public IActionResult SubmitTag(AddTagRequest addTagRequest)
         {
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
-           // var name = Request.Form["name"];    
-            //var displayName = Request.Form["displayname"];
+            //Mapping the AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
 
+            _blogdbcontext.tbl_Tags.Add(tag);
+            _blogdbcontext.SaveChanges();
             return View("Add");
         }
         
