7a6f2c9d930fc40a7854bf253d26cd1bef282e89
Mubashar Yasin <mubasharraj012@gmail.com>
2026-04-16 00:56:59 +0500

    Edit and Update Tag Functionality

--- Blog/Blog.Web/Controllers/AdminTagsController.cs
+++ Blog/Blog.Web/Controllers/AdminTagsController.cs
@@ -32,16 +32,65 @@ namespace Blog.Web.Controllers
 
             blogdbcontext.tbl_Tags.Add(tag);
             blogdbcontext.SaveChanges();
-            return View("Add");
+            return RedirectToAction("ListallTags");
         }
 
-        //[ActionName("List")]
         public IActionResult ListallTags()
         {
             //use dbcontext to read all the tags
             var tags = blogdbcontext.tbl_Tags.ToList();
-
             return View(tags);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            //1st Method 
+            // var tags = blogdbcontext.tbl_Tags.Find(id);
+
+            //2nd Method
+            var tag = blogdbcontext.tbl_Tags.FirstOrDefault(x => x.Id == id);
+
+            if(tag!=null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+                return View(editTagRequest);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+
+            var existingTag = blogdbcontext.tbl_Tags.Find(tag.Id);
+
+            if(existingTag!=null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                //save changes 
+                blogdbcontext.SaveChanges();
+
+                //show success notification
+                return RedirectToAction("Edit", new { id = editTagRequest.Id });
+
+            }
+            //Show error notification
+            return RedirectToAction("Edit" , new {id=editTagRequest.Id});
         }
         
     }
