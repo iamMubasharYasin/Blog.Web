590ffb9d43f45d9637f076c9c67ff5b8a766ec82
Mubashar Yasin <mubasharraj012@gmail.com>
2026-04-22 20:22:20 +0500

    Create ITagRepository.cs
    Declare Methods to access the DB.

---
+++ Blog/Blog.Web/Repositories/ITagRepository.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Web.Models.Domain;
+
+namespace Blog.Web.Repositories
+{
+    public interface ITagRepository
+    {
+        Task<IEnumerable<Tag>> GetAllAsync();
+        Task<Tag?> GetByIdAsync(Guid id);
+
+        Task<Tag> AddAsync(Tag tag);
+        Task<Tag?> UpdateAsync(Tag tag);
+        Task<Tag?> DeleteAsync(Guid id);
+    }
+}
