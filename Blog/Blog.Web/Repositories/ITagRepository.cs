685ec2f77434daacaf31125d059bde0c452944c4
Mubashar Yasin <mubasharraj012@gmail.com>
2026-04-22 23:52:26 +0500

    Update ITagRepository.cs

--- Blog/Blog.Web/Repositories/ITagRepository.cs
+++ Blog/Blog.Web/Repositories/ITagRepository.cs
@@ -5,7 +5,7 @@ namespace Blog.Web.Repositories
     public interface ITagRepository
     {
         Task<IEnumerable<Tag>> GetAllAsync();
-        Task<Tag?> GetByIdAsync(Guid id);
+        Task<Tag?> GetAsync(Guid id);
         Task<Tag> AddAsync(Tag tag);
         Task<Tag?> UpdateAsync(Tag tag);
         Task<Tag?> DeleteAsync(Guid id);
