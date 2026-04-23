baae197a6ab4eed4b1c7b77403819d830a38a32a
Mubashar Yasin <mubasharraj012@gmail.com>
2026-04-23 11:25:39 +0500

    Create New Controller
    Add Method (GET) and Create a New View of it.

---
+++ Blog/Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
