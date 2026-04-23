c430239f52e4e47aa7d7b80da4da423d58d1f7c8
Mubashar Yasin <mubasharraj012@gmail.com>
2026-04-23 13:02:58 +0500

    Create New Form to Add BlogPosts.
    Add two new Attributes in ViewModels to show the tags in the form of BlogPosts as a Dropdown

--- Blog/Blog.Web/Controllers/AdminBlogPostsController.cs
+++ Blog/Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Blog.Web.Controllers
 {
     public class AdminBlogPostsController : Controller
     {
-        public IActionResult Add()
+        ITagRepository tagRepository;
+
+        public AdminBlogPostsController(ITagRepository _tagRepository)
         {
-            return View();
+            tagRepository = _tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            //get all tags from the repo
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostsRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString()})
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Add(AddBlogPostsRequest addBlogPostsRequest)
+        {
+            return RedirectToAction("Add");
         }
     }
 }
---
+++ Blog/Blog.Web/Models/ViewModels/AddBlogPostsRequest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.Web.Models.ViewModels
+{
+    public class AddBlogPostsRequest
+    {
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageURL { get; set; }
+        public string URLHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public Boolean Visible { get; set; }
+
+        // Display Tags
+        public IEnumerable<SelectListItem> Tags { get; set; }
+
+        // Collect Tags
+        public string SelectedTag { get; set; }
+    }
+}
