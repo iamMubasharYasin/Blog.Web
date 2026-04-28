18f144d6bb008958d2f6bb912a76e85fac375c06
Mubashar Yasin <mubasharraj012@gmail.com>
2026-04-28 10:59:28 +0500

    Register to Cloudinary
    Connect Cloudinary API in App

--- Blog/Blog.Web/Controllers/ImagesController.cs
+++ Blog/Blog.Web/Controllers/ImagesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 
@@ -8,10 +10,41 @@ namespace Blog.Web.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
-        [HttpPost]
-        public async Task<IActionResult> UploadAsync(IFormFile formFile)
+        private IConfiguration configuration;
+        private Account account;
+
+        public ImagesController(IConfiguration configuration)
         {
-             //call the repository
+            this.configuration = configuration;
+            account = new Account(
+            configuration.GetSection("Cloudinary")["CloudName"],
+            configuration.GetSection("Cloudinary")["ApiKey"],
+            configuration.GetSection("Cloudinary")["ApiSecret"]);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            var client = new Cloudinary(account);
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),
+                DisplayName = file.FileName
+            };
+
+            var uploadResult = await client.UploadAsync(uploadParams);
+
+            if (uploadResult != null && uploadResult.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return Ok(uploadResult.SecureUrl.ToString());
+            }
+            return BadRequest("Image upload failed");
         }
     }
 }
