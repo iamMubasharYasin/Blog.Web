1eebd906ee23fc0e224cb1a4a35ea6651ba60d6c
Mubashar Yasin <mubasharraj012@gmail.com>
2026-05-08 15:29:03 +0500

    Displaying Tags On Home Page

--- Blog/Blog.Web/Controllers/HomeController.cs
+++ Blog/Blog.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Blog.Web.Models;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
 using Blog.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +10,26 @@ namespace Blog.Web.Controllers
     public class HomeController : Controller
     {
         IBlogPostsRepository blogPostsRepository;
-        public HomeController(IBlogPostsRepository blogPostsRepository)
+        ITagRepository tagRepository;
+        public HomeController(IBlogPostsRepository blogPostsRepository ,ITagRepository tagRepository)
         {
             this.blogPostsRepository = blogPostsRepository;
+            this.tagRepository = tagRepository;
+            
         }
         public async Task<IActionResult> Index()
         {
+            //getting all blogs
            var blogPosts =  await blogPostsRepository.GetAllAsync();
 
-            return View(blogPosts);
+           var tag =  await tagRepository.GetAllAsync();
+
+            var model = new HomeViewModel
+            {
+                BlogPosts = blogPosts,
+                Tag = tag
+            };
+            return View(model);
         }
 
 
