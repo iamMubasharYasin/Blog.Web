28f8c0ede1be536bde7c61d7ccdab8da30df4d93
Mubashar Yasin <mubasharraj012@gmail.com>
2026-05-09 00:11:48 +0500

    Install Identity Packages
    Identify the Roles(User,Admin,SuperAdmin)

---
+++ Blog/Blog.Web/Data/AuthDbContext.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Seed (User,Admin,SuperAdmin)
+            var adminRoleId = "221b7fbd-593e-4856-9bfb-a5dcfba257d0";
+            var userRoleId = "ff7ee110-0ed6-4127-a551-7fa914bba52f";
+            var superadminRoleId = "221b7fbd-593e-4856-9bfb-a5dcfba257d0";
+
+            var roles = new List<IdentityRole>
+            {
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "Admin",
+                Id = adminRoleId,
+                ConcurrencyStamp = adminRoleId
+            },
+
+             new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "User",
+                Id = userRoleId,
+                ConcurrencyStamp = userRoleId
+            },
+
+             new IdentityRole
+            {
+                Name = "SuperAdmin",
+                NormalizedName = "SuperAdmin",
+                Id = userRoleId,
+                ConcurrencyStamp = userRoleId
+            },
+
+        };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            //Seed SuperAdmin
+            var superAdminId = "6857563a-c93e-469b-a343-e8cf03b31875";
+            var SuperAdminUser = new IdentityUser
+            {
+                UserName = "superadmin@blog.com",
+                Email = "superadmin@blog.com",
+                NormalizedEmail = "superadmin@blog.com".ToUpper(),
+                NormalizedUserName = "superadmin@blog.com".ToUpper(),
+                Id = superAdminId
+            };
+
+            SuperAdminUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(SuperAdminUser, "Superadmin@123");
+             builder.Entity<IdentityRole>().HasData(SuperAdminUser);
+
+            //Mapping SuperAdmin to User & Admin
+            //Add All roles to SuperAdminUser
+            var superAdminRoles = new List<IdentityUserRole<string>>
+            {
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = userRoleId,
+                    UserId = superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = superadminRoleId,
+                    UserId = superAdminId
+                },
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
+        }
+
+       
+    }
+}
---
+++ Blog/Blog.Web/Data/IdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class IdentityDbContext
+    {
+        private DbContextOptions options;
+
+        public IdentityDbContext(DbContextOptions options)
+        {
+            this.options = options;
+        }
+    }
+}
