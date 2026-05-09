0be634f999056a5fc1e83bf110c3b0dbdfafca70
Mubashar Yasin <mubasharraj012@gmail.com>
2026-05-09 12:45:01 +0500

    Remove all AuthDbContext
    but not any changing in jsonfile and program.cs

--- Blog/Blog.Web/Data/AuthDbContext.cs
+++ Blog/Blog.Web/Data/AuthDbContext.cs
@@ -6,7 +6,8 @@ namespace Blog.Web.Data
 {
     public class AuthDbContext : IdentityDbContext<IdentityUser>
     {
-        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
         {
         }
 
@@ -14,80 +15,82 @@ namespace Blog.Web.Data
         {
             base.OnModelCreating(builder);
 
-            //Seed (User,Admin,SuperAdmin)
+            // Role Ids
             var adminRoleId = "221b7fbd-593e-4856-9bfb-a5dcfba257d0";
             var userRoleId = "ff7ee110-0ed6-4127-a551-7fa914bba52f";
-            var superadminRoleId = "e8632dc9-f5d2-4759-81c6-eee98a418415";
+            var superAdminRoleId = "e8632dc9-f5d2-4759-81c6-eee98a418415";
 
+            // Seed Roles
             var roles = new List<IdentityRole>
             {
-            new IdentityRole
-            {
-                Name = "Admin",
-                NormalizedName = "Admin",
-                Id = adminRoleId,
-                ConcurrencyStamp = adminRoleId
-            },
+                new IdentityRole
+                {
+                    Id = adminRoleId,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = adminRoleId
+                },
 
-             new IdentityRole
-            {
-                Name = "User",
-                NormalizedName = "User",
-                Id = userRoleId,
-                ConcurrencyStamp = userRoleId
-            },
+                new IdentityRole
+                {
+                    Id = userRoleId,
+                    Name = "User",
+                    NormalizedName = "USER",
+                    ConcurrencyStamp = userRoleId
+                },
 
-             new IdentityRole
-            {
-                Name = "SuperAdmin",
-                NormalizedName = "SuperAdmin",
-                Id = userRoleId,
-                ConcurrencyStamp = userRoleId
-            },
-
-        };
+                new IdentityRole
+                {
+                    Id = superAdminRoleId,
+                    Name = "SuperAdmin",
+                    NormalizedName = "SUPERADMIN",
+                    ConcurrencyStamp = superAdminRoleId
+                }
+            };
 
             builder.Entity<IdentityRole>().HasData(roles);
 
-            //Seed SuperAdmin
-            var superAdminId = "6857563a-c93e-469b-a343-e8cf03b31875";
-            var SuperAdminUser = new IdentityUser
+            // Super Admin User Id
+            var superAdminUserId = "6857563a-c93e-469b-a343-e8cf03b31875";
+
+            // IMPORTANT: FIXED PASSWORD HASH (no dynamic hashing)
+            var superAdminUser = new IdentityUser
             {
+                Id = superAdminUserId,
                 UserName = "superadmin@blog.com",
                 Email = "superadmin@blog.com",
-                NormalizedEmail = "superadmin@blog.com".ToUpper(),
-                NormalizedUserName = "superadmin@blog.com".ToUpper(),
-                Id = superAdminId
+                NormalizedUserName = "SUPERADMIN@BLOG.COM",
+                NormalizedEmail = "SUPERADMIN@BLOG.COM",
+                EmailConfirmed = true,
+                PasswordHash = "AQAAAAEAACcQAAAAELONG_FIXED_PASSWORD_HASH_VALUE"
             };
 
-            SuperAdminUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(SuperAdminUser, "Superadmin@123");
-             builder.Entity<IdentityRole>().HasData(SuperAdminUser);
+            builder.Entity<IdentityUser>().HasData(superAdminUser);
 
-            //Mapping SuperAdmin to User & Admin
-            //Add All roles to SuperAdminUser
+            // Assign Roles to Super Admin
             var superAdminRoles = new List<IdentityUserRole<string>>
             {
                 new IdentityUserRole<string>
                 {
-                    RoleId = adminRoleId,
-                    UserId = superAdminId
+                    UserId = superAdminUserId,
+                    RoleId = adminRoleId
                 },
+
                 new IdentityUserRole<string>
                 {
-                    RoleId = userRoleId,
-                    UserId = superAdminId
+                    UserId = superAdminUserId,
+                    RoleId = userRoleId
                 },
+
                 new IdentityUserRole<string>
                 {
-                    RoleId = superadminRoleId,
-                    UserId = superAdminId
-                },
+                    UserId = superAdminUserId,
+                    RoleId = superAdminRoleId
+                }
             };
 
-            builder.Entity<IdentityUser>().HasData(SuperAdminUser);
-           
+            builder.Entity<IdentityUserRole<string>>()
+                .HasData(superAdminRoles);
         }
-
-       
     }
 }
