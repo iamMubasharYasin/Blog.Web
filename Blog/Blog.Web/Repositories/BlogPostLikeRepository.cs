f31ae129b1a8b3a49980add882dfe5139447f192
Mubashar Yasin <mubasharraj012@gmail.com>
2026-05-30 20:13:00 +0500

    BlogPostLikeRepository
    not completed yet

---
+++ Blog/Blog.Web/Repositories/BlogPostLikeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Web.Data;
+
+namespace Blog.Web.Repositories
+{
+    public class BlogPostLikeRepository : IBlogPostLikeRepository
+    {
+        BlogDbContext blogDbContext;
+        public BlogPostLikeRepository(BlogDbContext _blogDbContext)
+        {
+            blogDbContext = _blogDbContext;
+        }
+
+        public Task<int> GetTotalLikes(Guid blogPostId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Blog/Blog.Web/Repositories/IBlogPostLikeRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog.Web.Repositories
+{
+    public interface IBlogPostLikeRepository
+    {
+        Task<int> GetTotalLikes(Guid blogPostId);
+    }
+}
